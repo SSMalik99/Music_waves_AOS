d372fae5cb66b22270fe445add6d00862fa4b517
kuldeepkaur <kuldeep12950@gmail.com>
2023-04-06 22:48:20 -0400

    LoginScreen added RegisterScreen added

---
+++ app/src/main/java/com/example/music_waves_aos/RegisterScreen.kt
@@ -0,0 +1,11 @@
+package com.example.music_waves_aos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen)
+    }
+}
